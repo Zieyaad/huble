7e94ddb55ca2ee85534f69fdede22885a72be526
Zieyaad Moses <zieyaad.moses@zooma.agency>
2023-11-05 19:46:46 +0200

    general layout of calculator

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <Hero />
+  <Calculator />
 </template>
 
 <script setup>
 import Hero from './components/Hero.vue'
+import Calculator from './components/Calculator.vue'
 </script>
--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -1,7 +1,31 @@
 <template>
-  <section class="Block"></section>
+  <section id="calculator">
+    <input v-model="input">
+    <br>
+    <button @click="appendToInput('+')">+</button>
+    <button @click="appendToInput('-')">-</button>
+    <button @click="appendToInput('*')">*</button>
+    <button @click="appendToInput('/')">/</button>
+    <br>
+    <button @click="clearInput">C</button>
+    <button @click="calculateResult">=</button>
+    <br>
+    <div id="calculation">{{ input }}</div>
+    <div id="total">{{ total }}</div>
+  </section>
 </template>
 
-<script></script>
+<script>
+export default {
+  data() {
+    return {
+      input: '',
+      calculation: '',
+      total: '',
+    };
+  },
+};
+</script>
 
-<style scoped lang="scss"></style>
+<style scoped>
+</style>
