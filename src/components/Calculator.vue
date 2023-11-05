0c308c4ce4e312c9f176069ee21e2bd70d804dba
Zieyaad Moses <zieyaad.moses@zooma.agency>
2023-11-05 22:45:18 +0200

    adding calculate total - a bit buggy

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -10,10 +10,8 @@
       </div>
     </div>
     <div class="flex flex-col justify-center p-7 border-[#013562] border-2 rounded-lg">
-      <div id="calculation" class="text-[#013562] font-bold text-3xl text-center mb-5 break-words">{{ calculation ? calculation : '0' }}</div>
-      <div id="total" class="text-[#013562] font-bold text-3xl text-center break-words">Result {{ total }}</div>
-
-
+      <div id="calculation" class="text-[#013562] font-bold text-3xl text-center mb-5 break-words">{{ calculation ? calculation : 0 }}</div>
+      <div id="total" class="text-[#013562] font-bold text-3xl text-center break-words">Result: {{ total }}</div>
     </div>
   </section>
 </template>
@@ -24,18 +22,34 @@ export default {
     return {
       numericInput: '',
       calculation: '',
-      total: '',
-    };
+      total: null,
+    }
   },
   methods: {
     validateNumericInput() {
       // Remove any non-numeric characters
-      this.numericInput = this.numericInput.replace(/[^0-9]/g, '');
+      this.numericInput = this.numericInput.replace(/[^0-9]/g, '')
     },
     appendToInput(operator) {
       if (this.numericInput) {
-        this.calculation += this.numericInput + operator;
-        this.numericInput = ''; // Clear the numeric input
+        this.calculation += this.numericInput + operator
+        // Clear the numeric input
+        this.numericInput = ''
+        // Calculate the total after appending an operator
+        this.calculateTotal()
+      }
+    },
+    calculateTotal() {
+      let calculation = this.calculation;
+      if (calculation.endsWith('+') || calculation.endsWith('-') || calculation.endsWith('*') || calculation.endsWith('/')) {
+        // Remove the trailing operator
+        calculation = calculation.slice(0, -1);
+      }
+      try {
+        // Evaluate the total
+        this.total = eval(calculation);
+      } catch (error) {
+        this.total = 'Error';
       }
     },
   },
