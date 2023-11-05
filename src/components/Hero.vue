4380ac0f2d6698de09a6435c9817e6387991519e
Zieyaad Moses <zieyaad.moses@zooma.agency>
2023-11-05 13:24:52 +0200

    adding hero component

---
+++ src/components/Hero.vue
@@ -0,0 +1,23 @@
+<template>
+  <section class="Block Block--Hero">
+    <div class="Block-Mask"></div>
+    <div class="Block-Content">
+      <p class="subtitle">Monolith</p>
+      <h1>Use AI software to test less and learn more.</h1>
+      <p>
+        Spend less time running expensive, repetitive tests and more time learning
+        from your engineering data to predict the exact tests to run.
+      </p>
+    </div>
+    <div class="Block-Footer">
+      <a class="button primary" href=":javascript;">Request demo</a>
+      <a class="button secondary" href=":javascript;">Speak to our team</a>
+    </div>
+  </section>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+</style>
