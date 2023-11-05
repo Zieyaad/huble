dd8d44ecee6ced514ad5dda7b1db0688c63637fc
Zieyaad Moses <zieyaad.moses@zooma.agency>
2023-11-05 14:11:43 +0200

    hero updates, container styling, adding variables

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,17 +1,21 @@
 <template>
-  <section class="Block Block--Hero">
-    <div class="Block-Mask"></div>
-    <div class="Block-Content">
-      <p class="subtitle">Monolith</p>
-      <h1>Use AI software to test less and learn more.</h1>
-      <p>
-        Spend less time running expensive, repetitive tests and more time learning
-        from your engineering data to predict the exact tests to run.
-      </p>
+  <section class="Block">
+    <div class="Block-Mask">
+      <img src="../assets/img/hero-bg.png" loading="eager" alt="hero-bg" width="1600"/>
     </div>
-    <div class="Block-Footer">
-      <a class="button primary" href=":javascript;">Request demo</a>
-      <a class="button secondary" href=":javascript;">Speak to our team</a>
+    <div class="Container">
+      <div class="Block-Content">
+        <p class="subtitle">Monolith</p>
+        <h1>Use AI software to test less and learn more.</h1>
+        <p>
+          Spend less time running expensive, repetitive tests and more time learning
+          from your engineering data to predict the exact tests to run.
+        </p>
+      </div>
+      <div class="Block-Footer">
+        <a class="button primary" href=":javascript;">Request demo</a>
+        <a class="button secondary" href=":javascript;">Speak to our team</a>
+      </div>
     </div>
   </section>
 </template>
@@ -19,5 +23,28 @@
 <script>
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+  .Block {
+    position: relative;
+    display: flex;
+    min-height: 700px;
+    // Mask
+    &-Mask {
+      position: absolute;
+      top: 0;
+      right: 0;
+      bottom: 0;
+      left: 0;
+      background-color: rgba(0, 0, 0, 0.4);
+      z-index: -1;
+
+      img {
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+        object-position: center center;
+      }
+    }
+  }
 </style>
